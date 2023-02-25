934d29545104051836dffc5dc4afad33a870bfef
Fitz Ignacio <ignacio.fitz@gmail.com>
2023-02-25 15:52:05 -0800

    commit tree 39aabbc19a44b05f060049a605dbae9d1d68ddbc

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
 }
 
 /* 
